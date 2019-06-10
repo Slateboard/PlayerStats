58e9bf6a7663b4dbca59a732676045fc4a962fc9
Ramses Brown <Slateboard@gmail.com>
2019-06-10 11:56:36 -0500

    Starting

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ namespace PlayerStats
 {
     class Program
     {
+
+        int health, strength, defense, luck;
+        string name, style;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
         }
     }
 }
